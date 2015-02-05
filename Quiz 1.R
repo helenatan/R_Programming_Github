abc7a9da4212d688660f14082ac42f6146ea6531
helenatan <Helena@hangs-mbp.attlocal.net>
2015-02-04 19:26:50 -0800

    2

--- Quiz 1.R
+++ Quiz 1.R
@@ -1,1 +1,7 @@
-Quiz 1 Solution
+##Question 1: 
+##The R language is a dialect of which of the following programming languages?
+##Answer: S
+
+##Question2:
+##The definition of free software consists of four freedoms (freedoms 0 through 3). Which of the following is NOT one of the freedoms that are part of the definition?
+
